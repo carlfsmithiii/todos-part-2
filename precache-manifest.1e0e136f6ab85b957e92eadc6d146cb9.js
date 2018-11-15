1284d0b162011973b7c36ec01905bce5a3fe4fd1
Carl Smith <carlfsmithiii@gmail.com>
2018-11-15 17:09:36 -0500

    Updates

---
+++ precache-manifest.1e0e136f6ab85b957e92eadc6d146cb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7407e633cecbd8ba9ced",
+    "url": "/todos-part-2/static/css/main.2ca97534.chunk.css"
+  },
+  {
+    "revision": "7407e633cecbd8ba9ced",
+    "url": "/todos-part-2/static/js/main.7407e633.chunk.js"
+  },
+  {
+    "revision": "1ce0cbf72445225d14a6",
+    "url": "/todos-part-2/static/js/1.1ce0cbf7.chunk.js"
+  },
+  {
+    "revision": "56578612cadf6faa79ed",
+    "url": "/todos-part-2/static/js/runtime~main.56578612.js"
+  },
+  {
+    "revision": "6ee69a50ea48b731b9f4e8962b7186b8",
+    "url": "/todos-part-2/index.html"
+  }
+];
